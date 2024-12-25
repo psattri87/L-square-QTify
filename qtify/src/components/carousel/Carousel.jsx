4222ffe30ac3fda8124c61d763c81156cacb0002
psattri87 <psattri87@gmail.com>
2024-12-25 16:59:45 +0530

    update slider button functionality

--- qtify/src/components/carousel/Carousel.jsx
+++ qtify/src/components/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 // import { FaChevronCircleRight, FaChevronCircleLeft } from "react-icons/fa";
@@ -14,45 +14,53 @@ import { ApiContext } from "../../context/ApiContext";
 
 import Card from "../card/Card";
 
-const Controls = ({ data }) => {
-  const swiper = useSwiper();
-  useEffect(() => {
-    swiper.slideTo(0);
-    // eslint-disable-next-line
-  }, [data]);
-  return <></>;
-};
-
-function SlideNextButton() {
-  const swiper = useSwiper();
-  swiper.slideNext();
-}
+// const Controls = ({ data }) => {
+//   const swiper = useSwiper();
+//   useEffect(() => {
+//     swiper.slideTo(7);
+//     // eslint-disable-next-line
+//   }, [data]);
+//   return <></>;
+// };
 
 export default function Carousel({ data, type }) {
-  const swiper = useSwiper();
+  const [currentSlide, setCurrentSlide] = useState(0);
   const { setSong } = useContext(ApiContext);
-  setSong(data[0])
+  const sliderRef = useRef(null);
+  const swiperRef = useRef();
+  
+  setSong(data[0]);
   const handleSongClick = (song) => {
     setSong(song);
   };
 
-  // useEffect(() => {
-  //   setSong(data[0]);
-  // }, []);
+  const handleNext = () => {
+    if (currentSlide < data.length-7) {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  useEffect(() => {
+    console.log("current slide is:", currentSlide);
+    sliderRef.current.swiper.slideTo(currentSlide, 500, false);
+  }, [currentSlide]);
 
   return (
     <div className={styles.carousel}>
       <Swiper
+        ref={sliderRef}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={40}
         slidesPerView={7}
-        // navigation
-        navigation={{
-          prevEl: ".swiperButtonNext",
-          nextEl: ".swiperButtonPrev",
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
         }}
-        // onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
         className={styles.swiper}
       >
         {data.map((song) => {
@@ -63,14 +71,14 @@ export default function Carousel({ data, type }) {
           );
         })}
       </Swiper>
-        <div className={styles.swiperButtons}>
-          <div className={styles.swiperButtonPrev} onClick={()=>console.log("clicked previous button")}>
-            <LeftArrow />
-          </div>
-          <div className={styles.swiperButtonNext} onClick={SlideNextButton}>
-            <RightArrow />
-          </div>
+      <div className={styles.swiperButtons}>
+        <div className={styles.swiperButtonPrev} onClick={handlePrevious}>
+          <LeftArrow />
         </div>
+        <div className={styles.swiperButtonNext} onClick={handleNext}>
+          <RightArrow />
+        </div>
+      </div>
     </div>
   );
 }
